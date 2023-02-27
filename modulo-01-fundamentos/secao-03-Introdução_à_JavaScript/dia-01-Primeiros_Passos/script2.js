772d6a5b534a47646ead2e77dab23cae9265e646
Joseph Bezerra <joseph.carlosx@gmail.com>
2023-02-27 18:55:10 -0400

    Exercicio 4

---
+++ modulo-01-fundamentos/secao-03-Introdução_à_JavaScript/dia-01-Primeiros_Passos/script2.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 12;
+let c = 500;
+
+if(a > b && a > c){
+    console.log(a+' é o maior dos 3');
+}else if(b > c){
+    console.log(b+" é o maior dos 3");
+}else{
+    console.log(c+" é o maior dos 3");
+}
