922a730a8282766e74aa8a8a9840c465dfe59a13
Joseph Bezerra <joseph.carlosx@gmail.com>
2023-02-27 18:51:14 -0400

    Exercicios 1 2 e 3

---
+++ modulo-01-fundamentos/secao-03-Introdução_à_JavaScript/dia-01-Primeiros_Passos/script.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = 7;
+
+let adição = a + b;
+let subtracao = a - b;
+let multiplicação = a*b;
+let divisao = a/b;
+let modulo = a%b;
+
+//console.log(adição);
+//console.log(subtracao);
+//console.log(multiplicação);
+//console.log(divisao);
+//console.log(modulo);
+
+if (a > b){
+    console.log(a+' é maior que '+b)
+}
+else{
+    console.log(b+' é maior que '+a)
+}
