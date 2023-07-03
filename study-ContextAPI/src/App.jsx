00268b4db1969399f354c67ae092660095281ed2
Joseph Pereira <joseph.carlosx@gmail.com>
2023-07-03 14:37:02 -0400

    Começando estudos de ContextAPI

---
+++ study-ContextAPI/src/App.jsx
@@ -0,0 +1,16 @@
+// /src/App.js
+
+import ThemeContext from './context/ThemeContext';
+// ...
+
+export default function App() {
+  return (
+    <ThemeContext.Provider value={{ color: 'dark' }}>
+      <div>
+        <Header />
+        <Image />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
