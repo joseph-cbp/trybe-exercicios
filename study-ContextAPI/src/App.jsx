64f63aba5362a9c96ef3abd415cf828c85357e44
Joseph Pereira <joseph.carlosx@gmail.com>
2023-07-04 14:59:55 -0400

    adicionado dark mode

--- study-ContextAPI/src/App.jsx
+++ study-ContextAPI/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Footer from "./components/Footer";
 import ThemeContext from "./context/ThemeContext";
-
+import Header from "./components/Header";
+import './App.css'
 export default function App() {
   const [themeColor, setThemeColor] = useState('dark');
   const toggleTheme = () => {
@@ -9,9 +10,8 @@ export default function App() {
   }
   return (
     <ThemeContext.Provider value={{ color: themeColor, toggleTheme}}>
-      <div>
+       <div className={ themeColor }>
         <Header />
-        <Image />
         <Footer />
       </div>
     </ThemeContext.Provider>
